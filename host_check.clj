f61f318fe94768b200425ce039d52d3fba30990b
ntestoc3 <ntestoc3@outlook.com>
2020-11-23 09:30:38 +0800

    添加host-check

---
+++ host_check.clj
@@ -0,0 +1,67 @@
+(ns host-check
+  (:require [burp-clj.utils :as utils]
+            [burp-clj.extender :as extender]
+            [burp-clj.message-viewer :as message-viewer]
+            [burp-clj.context-menu :as context-menu]
+            [burp-clj.scripts :as scripts]
+            [burp-clj.helper :as helper]
+            [seesaw.core :as gui]
+            [taoensso.timbre :as log]))
+
+
+(def cols-info [{:key :index :text "#" :class java.lang.Long}
+                {:key :host :text "Host" :class java.lang.String}
+                {:key :request/url :text "URL" :class java.lang.String}
+                {:key :response/status :text "Resp.Status" :class java.lang.Long}
+                {:key :response.headers/content-length :text "Resp.Len" :class java.lang.String}
+                {:key :response.headers/content-type :text "Resp.type" :class java.lang.String}
+                {:key :port :text "PORT" :class java.lang.Long}
+                {:key :comment :text "Comment" :class java.lang.String}])
+
+(defonce ms (atom nil))
+
+(defn host-check
+  [msgs]
+  (reset! ms msgs))
+
+(def menu-context #{:message-editor-request
+                    :message-viewer-request
+                    :target-site-map-table
+                    :proxy-history})
+
+
+(defn host-check-menu []
+  (context-menu/make-context-menu
+   menu-context
+   (fn [invocation]
+     [(gui/menu-item :text "Send request to host-check"
+                     :enabled? true
+                     :listen [:action (fn [e]
+                                        (-> (context-menu/get-selected-messge invocation)
+                                            (host-check)))])])))
+
+(def reg (scripts/reg-script! :host-check
+                              {:name "host header check"
+                               :version "0.0.1"
+                               :min-burp-clj-version "0.4.4"
+                               :context-menu {:host-check (host-check-menu)}}))
+
+
+(comment
+
+  (def hs (extender/get-proxy-history))
+
+  (def datas (map-indexed (fn [idx v]
+                            (let [info (helper/parse-http-req-resp v)]
+                              (assoc info :index idx))) hs))
+
+  (def ds (atom datas))
+
+  (utils/show-ui (message-viewer/http-message-viewer
+                  {:datas ds
+                   :columns cols-info
+                   :setting-key :host-check/intruder
+                   }))
+
+  )
+
